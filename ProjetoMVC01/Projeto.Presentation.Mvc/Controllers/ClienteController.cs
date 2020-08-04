fbb79e9bb0dbc1d4245c6f334e9dedde9b588092
Luiza Fortes <luizafortes@gmail.com>
2020-08-04 12:38:28 -0300

    aula 11 - continuação do crud cliente

--- ProjetoMVC01/Projeto.Presentation.Mvc/Controllers/ClienteController.cs
+++ ProjetoMVC01/Projeto.Presentation.Mvc/Controllers/ClienteController.cs
@@ -23,7 +23,19 @@ namespace Projeto.Presentation.Mvc.Controllers
             if (ModelState.IsValid)
             {
                 try
-                {
+                {                    
+                    //verificar se o cpf do cliente já encontra-se cadastrado
+                    if (clienteRepository.GetByCpf(model.Cpf) != null)
+                    {
+                        throw new Exception("O CPF informado já existe.");
+                    }
+
+                    //verificar se o email do cliente já encontra-se cadastrado
+                    if (clienteRepository.GetByEmail(model.Email) != null)
+                    {
+                        throw new Exception("O email informado já encontra-se cadastrado. Informe outro email.");
+                    }
+
                     //criando um objeto da classe de entidade
                     var cliente = new Cliente();
                     cliente.Nome = model.Nome;
@@ -47,11 +59,51 @@ namespace Projeto.Presentation.Mvc.Controllers
         }
 
         //Abrir a página
-        public IActionResult Consulta()
+        public IActionResult Consulta([FromServices] ClienteRepository clienteRepository)
         {
-            return View();
+            var clientes = new List<Cliente>();
+            try
+            {
+                clientes = clienteRepository.GetAll();
+            }
+            catch (Exception e)
+            {
+                TempData["MensagemErro"] = "Erro: " + e.Message;
+            }
+            return View(clientes);
         }
 
+        //método de ação
+        public IActionResult Exclusao(int id, [FromServices] ClienteRepository clienteRepository)
+        {
+            try
+            {
+                var cliente = clienteRepository.GetById(id);
 
+                //verificar se o cliente foi obtido no banco de dados
+                if (cliente != null)
+                {
+                    //excluindo o cliente
+                    clienteRepository.Delete(cliente);
+                    TempData["MensagemSucesso"] = "Cliente excluído com sucesso.";                    
+                }
+                else
+                {
+                    throw new Exception("Cliente não encontrado.");
+                }
+            }
+            catch (Exception e)
+            {
+                TempData["MensagemErro"] = "Erro: " + e.Message;
+            }
+
+            //redirecionar de volta para a página de consulta.
+            return RedirectToAction("Consulta");
+        }
+
+        public IActionResult Edicao(int id)
+        {
+            return View(); //abrir uma página
+        }
     }
 }
