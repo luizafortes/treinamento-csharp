6e62ee6bce27fb64cbc93abc1b12e976b7fc78a8
Luiza Fortes <luizafortes@gmail.com>
2020-08-11 12:01:58 -0300

    aula 13 - Criando páginas de login e registro de usuário

--- ProjetoMVC02/Projeto.Presentation.Mvc/Controllers/AccountController.cs
+++ ProjetoMVC02/Projeto.Presentation.Mvc/Controllers/AccountController.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Projeto.Presentation.Mvc.Models;
 
 namespace Projeto.Presentation.Mvc.Controllers
 {
     public class AccountController : Controller
     {
+        //método que abre a página inicial de Login
         public IActionResult Login()
         {
+            return View();
+        }
+
+        //método que abre a página de Registro
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost] //método executado pelo SUBMIT do formulário
+        public IActionResult Login(AccountLoginModel model)
+        {
+            //verificar se todos os campos foram validados com sucesso
+            if (ModelState.IsValid)
+            {
+
+            }
+
             return View();
         }
     }
--- ProjetoMVC02/Projeto.Presentation.Mvc/Models/AccountRegisterModel.cs
+++ ProjetoMVC02/Projeto.Presentation.Mvc/Models/AccountRegisterModel.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace Projeto.Presentation.Mvc.Models
 {
-    public class AccountLoginModel
+    public class AccountRegisterModel
     {
+        [MinLength(6, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [MaxLength(100, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
+        public string Nome { get; set; }
+
         [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
         [Required(ErrorMessage = "Por favor, informe o email de acesso.")]
         public string Email { get; set; }
@@ -16,5 +20,9 @@ namespace Projeto.Presentation.Mvc.Models
         [MaxLength(20, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
         [Required(ErrorMessage = "Por favor, informe a senha de acesso.")]
         public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "Senhas não conferem")]
+        [Required(ErrorMessage = "Por favor, confirme a senha de acesso.")]
+        public string SenhaConfirmacao { get; set; }
     }
 }
